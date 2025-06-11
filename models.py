65bcd82d713adf6ce6a4879e5a5ab9f853da1545
Akshay Kumar <119182584+cautiousdanger@users.noreply.github.com>
2025-06-11 11:12:34 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,27 @@
 
+from pydantic import BaseModel, EmailStr
+from uuid import UUID
+
+class FitnessClassCreate(BaseModel):
+    name: str
+    dateTime: str
+    instructor: str
+    availableSlots: int
+
+class BookingCreate(BaseModel):
+    class_id: UUID
+    client_name: str
+    client_email: EmailStr
+
+class FitnessClassOut(BaseModel):
+    id: UUID
+    name: str
+    dateTime: str
+    instructor: str
+    availableSlots: int
+
+class BookingOut(BaseModel):
+    id: UUID
+    class_id: UUID
+    client_name: str
+    client_email: EmailStr
